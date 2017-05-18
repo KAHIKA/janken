16c0573dca6b1e09021dc296e56a5151aaf473f3
KAHIKA <kahika@net90-dhcp-138.sfc.keio.ac.jp>
2017-05-18 11:50:01 +0900

    第5回　練習4　3人に勝ちたい

--- docs/player.js
+++ docs/player.js
@@ -4,6 +4,24 @@ const PA = 2;
 
 let matches = 0;
 
+//チョキ大好きマンに対するアクション
+function actionAgainstChoki() {
+ return GU;
+}
+
+//表か裏かマンに対するアクション
+function actionAgainstOmoteUra(){
+   matches = matches + 1;
+  const amari = matches % 2;
+  const isEven = amari == 0;
+  console.log(`${matches} % 2 = ${amari} : ${isEven}`);
+  if(isEven){
+    result = GU;
+  }
+  return result;
+}
+
+//表か裏かマン2に対するアクション
 function actionAgainstOmoteUra2(){
  matches = matches + 1
  const amari = matches % 3;
@@ -13,9 +31,29 @@ function actionAgainstOmoteUra2(){
     return CHOKI
    }
    return GU;
- }
+}
+
+/*　対戦相手の名前
+チョキ大好き："fighter::choki-lover"
+表か裏："fighter::odd-even"
+表か裏2："fighter::on-third"
+*/
 
  function action(oppornent){
- let result = actionAgainstOmoteUra2();
- return result;
+ console.log(oppornent);
+ maches = matches + 1
+ let result = GU;
+ if (oppornent == "fighter::choki-lover"){
+   result = actionAgainstChoki();
+ }  else { 
+   if (oppornent == "fighter::odd-even" ){
+   result = actionAgainstOmoteUra();
+ } else {
+   result = actionAgainstOmoteUra2();
  }
+ }
+return result;
+
+}
+
+
